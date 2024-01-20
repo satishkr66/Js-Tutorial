d4c64885a96c01ec64709ab1a16f3150e0b0027a
Satish Kumar <87963940+satishkr66@users.noreply.github.com>
2024-01-20 11:28:05 +0000

    Done

---
+++ Basic_JS/Variables.js
@@ -0,0 +1,7 @@
+const accoutId=234343;
+let   accountHolderName="Satish Kumar";
+var  accountHolderAddress ="Chhotki Nawada Gaya Bihar India ";
+console.log("Welcome to JavaScript !");
+console.log(accoutId);
+console.log(accountHolderName);
+console.log(accountHolderAddress);
